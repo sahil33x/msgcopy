78a341eaf818792235e3eca189d0e09fd005741e
sahil33x <113387051+sahil33x@users.noreply.github.com>
2022-09-13 18:27:58 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,9 +3,9 @@ from pyromod import listen
 
 api_id = 8763712
 api_hash = "835d27216f117e22a5c192b89a4ce457"
-bot_token = "5310301280:AAGLExVt5XtDNM-N-shGt_hCYrRdftAhyQg"
+bot_token = "5794495646:AAHvPcaqBtRxHljAopb8cWt-RABTfL1r-Og"
 tg_session = None
-sudo_users = ["830785064", "872830003", "846709496", "927193767", "1542508017", "913846663", "983956963", "1700962147"]
+sudo_users = [830785064, 872830003, 846709496, 927193767, 1542508017, 913846663, 983956963, 1700962147]
 
 
 if tg_session:
@@ -13,8 +13,10 @@ if tg_session:
 elif bot_token:
   app = Client(":memory:", api_id, api_hash, bot_token=bot_token)
 else:
-  print('Give Token!')
+  print('Give Token Or Session!')
   sys.exit(1)
 
+
+
 with app:
-  print('Bot Started!')
+  uname = app.get_me().username
