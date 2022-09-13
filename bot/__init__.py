823b905a587408e1db71c7b1649fe43c7d29f372
sahil33x <113387051+sahil33x@users.noreply.github.com>
2022-09-13 18:34:17 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -3,7 +3,7 @@ from pyromod import listen
 
 api_id = 8763712
 api_hash = "835d27216f117e22a5c192b89a4ce457"
-bot_token = "5794495646:AAHvPcaqBtRxHljAopb8cWt-RABTfL1r-Og"
+bot_token = "5310301280:AAGLExVt5XtDNM-N-shGt_hCYrRdftAhyQg"
 tg_session = None
 sudo_users = [830785064, 872830003, 846709496, 927193767, 1542508017, 913846663, 983956963, 1700962147]
 
